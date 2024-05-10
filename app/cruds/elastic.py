1d1529b0e2c2ced5198c2b4b10e1161f552a4a87
Яна Бубнова <bujhvh@mail.ru>
2024-05-10 11:56:20 +0300

    start elastic crud

--- app/cruds/elastic.py
+++ app/cruds/elastic.py
@@ -2,10 +2,13 @@ from app.schemas.v1.films_schemas import GetFilmSchemaOut
 from app.schemas.v1.persons_schemas import GetPersonSchemaOut
 
 
-class SearchService:
+class ElasticCrud:
 
     async def search_films(self, query: str) -> list[GetFilmSchemaOut]:
         raise NotImplementedError
 
     async def search_persons(self, query: str) -> list[GetPersonSchemaOut]:
         raise NotImplementedError
+
+    async def get_films(self) -> list[GetFilmSchemaOut]:
+        pass
--- app/services/api/v1/films_service/films_service.py
+++ app/services/api/v1/films_service/films_service.py
@@ -4,19 +4,19 @@ from fastapi import Depends
 
 from app.schemas.v1.films_schemas import GetFilmExtendedSchemaOut, GetFilmSchemaOut
 from app.services.api.v1.base import BaseV1Service
-from app.services.elastic.search_service import SearchService
+from app.cruds.elastic import ElasticCrud
 
 
 class FilmsService(BaseV1Service):
 
-    def __init__(self, search_service: SearchService = Depends()):
-        self.search_service = search_service
+    def __init__(self, crud: ElasticCrud = Depends()):
+        self.crud = crud
 
     async def get_films(self, sort: str | None, genre: UUID | None) -> list[GetFilmSchemaOut]:
-        raise NotImplementedError
+        await self.crud.get_films()
 
     async def get_film(self, film_id: UUID) -> GetFilmExtendedSchemaOut:
         raise NotImplementedError
 
     async def search_films(self, query: str) -> list[GetFilmSchemaOut]:
-        return await self.search_service.search_films(query)
+        return await self.crud.search_films(query)
